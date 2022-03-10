4e301eb18b3b72a2b206b93eefc7d6b5ea55d3df
elbub1979 <ilya.leuh09@gmail.com>
2022-03-10 22:39:13 +0300

    add Unicode suits

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,4 @@
-value = %w[2 3 4 5 6 7 8 9 10 J Q K A J]
-suit = %w[Diamonds Hearts Clubs Spades]
+values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
+suits = ['♦', '♠', '♥ ', '♣']
 
-puts "#{value.sample} of #{suit.sample}"
+puts "#{values.sample} of #{suits.sample}"
